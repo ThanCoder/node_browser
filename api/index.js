0ad10e82a2e8d0588ef857be668de3f45b655ec7
thancoder <thancoder2019@gmail.com>
2025-04-16 08:30:56 +0630

    update

--- api/index.js
+++ api/index.js
@@ -3,9 +3,9 @@ const puppeteer = require("puppeteer-core");
 const chromium = require("@sparticuz/chromium");
 const { headersArray } = require("puppeteer-core");
 
-// const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 
-// puppeteer.use(StealthPlugin());
+puppeteer.use(StealthPlugin());
 
 const app = express();
 
